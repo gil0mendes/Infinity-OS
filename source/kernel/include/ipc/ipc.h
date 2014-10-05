c58377ef7b2ecac8ad893c3ab57999963092489f
Gil Mendes (gil0mendes) <gil00mendes@gmail.com>
2014-10-05 19:22:03 +0100

    Various changes to IPC interface
    Simplified things a bit, removed connection payload messages and
    accept, reject and forward. Connections now become active as soon as
    they are listened for. Rejection should just be part of the message
    protocol - a server can send an error message back to the client after
    listening and
    close the connection if it wishes to reject it, no need for the kernel
    to provide special mechanisms for this.
    
    Similarly payload messages can just be sent by the client after opening
    the connection. The main reason I added them in the first place was for
    forwarding - forwarding was going to be used for connecting to services
    via the root port, payload message would be used to specify service
    name and service manager would forward to the service's port. Instead
    the service manager will just provide a lookup call and return a handle
    to the service port, which the client can then connect to itself.
    
    Also added a couple of new things: messages now have a timestamp, and
    carry a copy of the security context of the thread that sent the
    message which can optionally be retrieved by kern_connection_receive().
    This means that servers can see changes to a client's security context,
    and allows for per-message access checks.

--- source/kernel/include/ipc/ipc.h
+++ source/kernel/include/ipc/ipc.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 Gil Mendes
+ * Copyright (C) 2013-2014 Gil Mendes
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -15,9 +15,9 @@
  */
 
 /**
- * @file
- * @brief		Kernel IPC interface.
- */
+* @file
+* @brief		Kernel IPC interface.
+*/
 
 #ifndef __IPC_IPC_H
 #define __IPC_IPC_H
@@ -41,27 +41,28 @@ struct ipc_port;
 
 /** Kernel-internal IPC message structure. */
 typedef struct ipc_kmessage {
-	list_t header;			/**< Link to message queue. */
-	refcount_t count;		/**< Reference count. */
-	ipc_message_t msg;		/**< Wrapped user message structure. */
-	void *data;			/**< Attached data (NULL if size is 0). */
-	object_handle_t *handle;	/**< Attached handle (can be NULL). */
+    list_t header;			/**< Link to message queue. */
+    refcount_t count;		/**< Reference count. */
+    ipc_message_t msg;		/**< Wrapped user message structure. */
+    security_context_t security;	/**< Security context that the message was sent with. */
+    void *data;			/**< Attached data (NULL if size is 0). */
+    object_handle_t *handle;	/**< Attached handle (can be NULL). */
 } ipc_kmessage_t;
 
 /** IPC endpoint structure. */
 typedef struct ipc_endpoint {
-	struct ipc_endpoint *remote;	/**< Other end of the connection. */
-	struct ipc_connection *conn;	/**< Connection structure. */
-	unsigned flags;			/**< Behaviour flags for the endpoint. */
+    struct ipc_endpoint *remote;	/**< Other end of the connection. */
+    struct ipc_connection *conn;	/**< Connection structure. */
+    unsigned flags;			/**< Behaviour flags for the endpoint. */
 
-	list_t messages;		/**< List of queued messages. */
-	size_t message_count;		/**< Number of messages in message queue. */
-	condvar_t space_cvar;		/**< Condition variable to wait for space. */
-	condvar_t data_cvar;		/**< Condition variable to wait for data. */
-	ipc_kmessage_t *pending;	/**< Message with pending data/handle. */
+    list_t messages;		/**< List of queued messages. */
+    size_t message_count;		/**< Number of messages in message queue. */
+    condvar_t space_cvar;		/**< Condition variable to wait for space. */
+    condvar_t data_cvar;		/**< Condition variable to wait for data. */
+    ipc_kmessage_t *pending;	/**< Message with pending data/handle. */
 
-	notifier_t hangup_notifier;	/**< Notifier for remote end being closed. */
-	notifier_t receive_notifier;	/**< Notifier for message arrival. */
+    notifier_t hangup_notifier;	/**< Notifier for remote end being closed. */
+    notifier_t message_notifier;	/**< Notifier for message arrival. */
 } ipc_endpoint_t;
 
 /** IPC endpoint behaviour flags. */
@@ -69,30 +70,28 @@ typedef struct ipc_endpoint {
 
 /** IPC connection structure. */
 typedef struct ipc_connection {
-	refcount_t count;		/**< Reference count. */
-	mutex_t lock;			/**< Lock for the connection. */
+    refcount_t count;		/**< Reference count. */
+    mutex_t lock;			/**< Lock for the connection. */
 
-	/** State of the connection. */
-	enum {
-		/** Connection is being set up. */
-		IPC_CONNECTION_SETUP,
+    /** State of the connection. */
+    enum {
+        /** Connection is being set up. */
+                IPC_CONNECTION_SETUP,
 
-		/** Connection is active. */
-		IPC_CONNECTION_ACTIVE,
+        /** Connection is active. */
+                IPC_CONNECTION_ACTIVE,
 
-		/** At least one side has closed the connection. */
-		IPC_CONNECTION_CLOSED,
-	} state;
+        /** At least one side has closed the connection. */
+                IPC_CONNECTION_CLOSED,
+    } state;
 
-	/** Endpoints for each end of the connection. */
-	ipc_endpoint_t endpoints[2];
+    /** Endpoints for each end of the connection. */
+    ipc_endpoint_t endpoints[2];
 
-	/** Data used during connection setup. */
-	list_t header;			/**< Link to connection list. */
-	condvar_t open_cvar;		/**< Condition for connecting thread to wait on. */
-	ipc_client_t *client;		/**< Pointer to client information structure. */
-	ipc_kmessage_t *payload;	/**< Payload message. */
-	status_t status;		/**< Setup status code. */
+    /** Data used during connection setup. */
+    list_t header;			/**< Link to connection list. */
+    condvar_t open_cvar;		/**< Condition for connecting thread to wait on. */
+    ipc_client_t *client;		/**< Pointer to client information structure. */
 } ipc_connection_t;
 
 /** Definitions for endpoint IDs. */
@@ -101,43 +100,42 @@ typedef struct ipc_connection {
 
 /** IPC port operations. */
 typedef struct ipc_port_ops {
-	/**
-	 * Handle a connection on a port.
-	 *
-	 * This function is called when a connection attempt is made to this
-	 * port. The given endpoint refers to the server side of the connection.
-	 * This function is called from the context of the thread making the
-	 * connection.
-	 *
-	 * Returning an error from this function has the effect of rejecting
-	 * the connection and causing that error to be returned to the client's
-	 * connect call. Returning STATUS_SUCCESS causes the connection to
-	 * become active.
-	 *
-	 * @param port		Port being connected to (locked).
-	 * @param endpoint	Endpoint for server side of the connection.
-	 * @param payload	Payload message (NULL if no payload).
-	 * @param timeout	Timeout in nanoseconds. 0 should return
-	 *			immediately if unable to connect without delay,
-	 *			-1 should block forever.
-	 *
-	 * @return		Status code describing result of the operation.
-	 */
-	status_t (*connect)(struct ipc_port *port, ipc_endpoint_t *endpoint,
-		ipc_kmessage_t *payload, nstime_t timeout);
+    /**
+    * Handle a connection on a port.
+    *
+    * This function is called when a connection attempt is made to this
+    * port. The given endpoint refers to the server side of the connection.
+    * This function is called from the context of the thread making the
+    * connection.
+    *
+    * Returning an error from this function has the effect of rejecting
+    * the connection and causing that error to be returned to the client's
+    * connect call. Returning STATUS_SUCCESS causes the connection to
+    * become active.
+    *
+    * @param port		Port being connected to (locked).
+    * @param endpoint	Endpoint for server side of the connection.
+    * @param timeout	Timeout in nanoseconds. 0 should return
+    *			immediately if unable to connect without delay,
+    *			-1 should block forever.
+    *
+    * @return		Status code describing result of the operation.
+    */
+    status_t (*connect)(struct ipc_port *port, ipc_endpoint_t *endpoint,
+            nstime_t timeout);
 } ipc_port_ops_t;
 
 /** IPC port structure. */
 typedef struct ipc_port {
-	mutex_t lock;			/**< Lock for structure. */
-	refcount_t count;		/**< References to the port. */
-	ipc_port_ops_t *ops;		/**< Operations for the port. */
-	void *private;			/**< Private data pointer. */
-	process_t *owner;		/**< Owning process. */
-	size_t owner_count;		/**< References from the owner. */
-	list_t waiting;			/**< List of in-progress connection attempts. */
-	condvar_t listen_cvar;		/**< Connection condition variable. */
-	notifier_t listen_notifier;	/**< Notifier for connection attempts. */
+    mutex_t lock;			/**< Lock for structure. */
+    refcount_t count;		/**< References to the port. */
+    ipc_port_ops_t *ops;		/**< Operations for the port. */
+    void *private;			/**< Private data pointer. */
+    process_t *owner;		/**< Owning process. */
+    size_t owner_count;		/**< References from the owner. */
+    list_t waiting;			/**< List of in-progress connection attempts. */
+    condvar_t listen_cvar;		/**< Connection condition variable. */
+    notifier_t connection_notifier;	/**< Notifier for connection attempts. */
 } ipc_port_t;
 
 /** Kernel internal IPC flags. */
@@ -149,27 +147,27 @@ extern void ipc_kmessage_retain(ipc_kmessage_t *msg);
 extern void ipc_kmessage_release(ipc_kmessage_t *msg);
 extern void ipc_kmessage_set_data(ipc_kmessage_t *msg, void *data, size_t size);
 extern void ipc_kmessage_set_handle(ipc_kmessage_t *msg,
-	object_handle_t *handle);
+        object_handle_t *handle);
 
 /** Check whether a message has attached data.
- * @param msg		Message to check.
- * @return		Whether data is attached. */
-static inline bool ipc_kmessage_has_attached(ipc_kmessage_t *msg) {
-	return (msg->data || msg->handle);
+* @param msg		Message to check.
+* @return		Whether data is attached. */
+static inline bool ipc_kmessage_has_attachment(ipc_kmessage_t *msg) {
+    return (msg->data || msg->handle);
 }
 
 extern void ipc_connection_close(ipc_endpoint_t *endpoint);
 extern status_t ipc_connection_send(ipc_endpoint_t *endpoint,
-	ipc_kmessage_t *msg, unsigned flags, nstime_t timeout);
+        ipc_kmessage_t *msg, unsigned flags, nstime_t timeout);
 extern status_t ipc_connection_receive(ipc_endpoint_t *endpoint, unsigned flags,
-	nstime_t timeout, ipc_kmessage_t **msgp);
+        nstime_t timeout, ipc_kmessage_t **msgp);
 
 extern ipc_port_t *ipc_port_create(ipc_port_ops_t *ops, void *private);
 extern void ipc_port_destroy(ipc_port_t *port);
 extern void ipc_port_retain(ipc_port_t *port);
 extern void ipc_port_release(ipc_port_t *port);
 extern status_t ipc_port_publish(ipc_port_t *port, handle_t *idp,
-	handle_t *uidp);
+        handle_t *uidp);
 
 extern void ipc_init(void);
 
